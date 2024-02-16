5eab2a6aa32f7a5cd603e05f14d19153115080d9
willofD13 <leonidastsiakas13@gmail.com>
2024-02-16 18:32:24 +0200

    Implement symbol placement and win check

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
-let Gameboard = (function() {
+let Game = (function() {
     let gameboard = document.querySelector('#gameboard');
-    for (i=0; i<9; i++) {
+    let turn = 0;
+    let o = 'O';
+    let x = 'X';
+    render();
+    let boxes  = document.querySelectorAll('.box')
+    wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+    
+    function render() {
+        for (i=0; i<9; i++) {
         let div = document.createElement('div');
         div.setAttribute('class', 'box');
+        div.setAttribute('id', `${i}`);
         gameboard.appendChild(div);
+        }
+    };
+
+    boxes.forEach(box => {
+        box.addEventListener('click', placeSymbol)
+    }) 
+
+    function placeSymbol(e) {
+        let symbol = turn % 2 === 0 ? x : o
+        e.target.innerHTML = symbol;
+        checkForWinner();
+        turn ++;
+    }
+
+    function checkForWinner() {
+        wins.forEach( win => {
+            if (document.getElementById(`${win[0]}`).textContent === document.getElementById(`${win[1]}`).textContent 
+                && document.getElementById(`${win[1]}`).textContent === document.getElementById(`${win[2]}`).textContent 
+                && document.getElementById(`${win[0]}`).textContent !== '') {
+                alert('We have a winner!');
+            }
+        })
     }
 })()
