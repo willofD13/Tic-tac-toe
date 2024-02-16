64338605b9cab09dc85ce1e58c732f35fd142f96
willofD13 <leonidastsiakas13@gmail.com>
2024-02-16 18:58:34 +0200

    Use boxes nodelist for win check

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let Game = (function() {
     let gameboard = document.querySelector('#gameboard');
+    render();
     let turn = 0;
     let o = 'O';
     let x = 'X';
-    render();
-    let boxes  = document.querySelectorAll('.box')
-    wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]]
+    let boxes  = document.querySelectorAll('.box');
+    let wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
     
     function render() {
         for (i=0; i<9; i++) {
@@ -17,7 +17,7 @@ let Game = (function() {
     };
 
     boxes.forEach(box => {
-        box.addEventListener('click', placeSymbol)
+        box.addEventListener('click', placeSymbol, { once: true })
     }) 
 
     function placeSymbol(e) {
@@ -29,9 +29,9 @@ let Game = (function() {
 
     function checkForWinner() {
         wins.forEach( win => {
-            if (document.getElementById(`${win[0]}`).textContent === document.getElementById(`${win[1]}`).textContent 
-                && document.getElementById(`${win[1]}`).textContent === document.getElementById(`${win[2]}`).textContent 
-                && document.getElementById(`${win[0]}`).textContent !== '') {
+            if (boxes[win[0]].textContent === boxes[win[1]].textContent 
+                && boxes[win[1]].textContent === boxes[win[2]].textContent 
+                && boxes[win[0]].textContent !== '') {
                 alert('We have a winner!');
                 clearGameboard();
             }
