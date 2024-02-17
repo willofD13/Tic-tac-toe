8758ab81bfd80775ddab8b2d19e84cc31ebf3680
willofD13 <leonidastsiakas13@gmail.com>
2024-02-17 17:13:53 +0200

    Make rendering a separate module

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
-let Game = (function() {
-    let gameboard = document.querySelector('#gameboard');
-    render();
-    let turn = 0;
-    let o = 'O';
-    let x = 'X';
-    let boxes  = document.querySelectorAll('.box');
-    let wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
-    
-    function render() {
-        for (i=0; i<9; i++) {
+const Render = (function() {
+    const gameboard = document.querySelector('#gameboard');
+
+    for (i=0; i<9; i++) {
         let div = document.createElement('div');
         div.setAttribute('class', 'box');
         div.setAttribute('id', `${i}`);
         gameboard.appendChild(div);
-        }
-    };
+    }
 
-    boxes.forEach(box => {
-        box.addEventListener('click', placeSymbol)
-    }) 
+})();
+
+const Game = (function() {
+    let turn = 0;
+    const o = 'O';
+    const x = 'X';
+    const boxes  = document.querySelectorAll('.box');
+    const wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
+    addEvent();
+
+    function addEvent() {
+        boxes.forEach(box => {
+        box.addEventListener('click', placeSymbol, { once: true })
+        }) 
+    }
 
     function placeSymbol(e) {
         let symbol = turn % 2 === 0 ? x : o
         e.target.innerHTML = symbol;
+
         if (checkForWinner(symbol)) {
             alert('We have a winner!');
             clearGameboard();
             reset();
-            }
+        }
+
         turn ++;
     }
 
