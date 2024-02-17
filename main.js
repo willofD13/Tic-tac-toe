042ff11a59ad4a4cbb4840a6394a560f3ccc100f
willofD13 <leonidastsiakas13@gmail.com>
2024-02-17 17:40:16 +0200

    Assign functions in const variables

--- main.js
+++ main.js
@@ -16,15 +16,8 @@ const Game = (function() {
     const x = 'X';
     const boxes  = document.querySelectorAll('.box');
     const wins = [[0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
-    addEvent();
 
-    function addEvent() {
-        boxes.forEach(box => {
-        box.addEventListener('click', placeSymbol, { once: true })
-        }) 
-    }
-
-    function placeSymbol(e) {
+    const placeSymbol = (e) => {
         let symbol = turn % 2 === 0 ? x : o
         e.target.innerHTML = symbol;
 
@@ -34,10 +27,16 @@ const Game = (function() {
             reset();
         }
 
-        turn ++;
+        changeTurn();
     }
 
-    function checkForWinner(symbol) {
+    const addEvent = () => {
+        boxes.forEach(box => {
+        box.addEventListener('click', placeSymbol, { once: true })
+        }) 
+    }
+
+    const checkForWinner = (symbol) => {
         return wins.some( win => {
             return win.every(index => {
                 return boxes[index].textContent === symbol
@@ -45,15 +44,21 @@ const Game = (function() {
         })
     }
 
-    function clearGameboard() {
+    const changeTurn = () => {
+        turn ++
+    }
+
+    const clearGameboard = () => {
         boxes.forEach(box => {
             box.textContent = '';
         }) 
     }
 
-    function reset() {
+    const reset = () => {
         clearGameboard();
         location.reload();
     }
+
+    addEvent();
 
 })()
